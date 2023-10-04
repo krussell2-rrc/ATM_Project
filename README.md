b4c45527708b4a63bf6809227e96d25d66b9db9c
krussell2 <krussell2@academic@rrc.ca>
2023-10-04 02:40:40 -0500

    Updated README.md file to include program name for interest_update.py along with it's intention

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ Kareem Russell
 ## Assignment
 
 ## ATM Simulator
-This program will make it possible for users to deposit and withdraw money from their bank account while managing their transactions. 
+This program will make it possible for users to deposit and withdraw money from their bank account while managing their transactions.
+
+## Interest Applier
+This program will apply interest to multiple customers' accounts based on their current balance.
--- bank_transaction.py
+++ bank_transaction.py
@@ -9,40 +9,25 @@ interface_values = ["W", "D", "Q"]
 
 import random
 users_bank_balance = random.randint(-1000, 10000)
-users_bank_balance = "4298"
 users_bank_balance = float(users_bank_balance)
 print(float(users_bank_balance))
 
 border = "****************************************"
 print(border)
-
-pixell = "PIXELL RIVER FINANCIAL"
-print(pixell.center(40))
-
-atm_simulator = "ATM Simulator"
-print(atm_simulator.center(40))
-
+print("PIXELL RIVER FINANCIAL".center(40))
+print("ATM Simulator".center(40))
 current_balance = (float(users_bank_balance))
 current_balance_message = (f"Your current balance is: ${current_balance}")
 print(current_balance_message.center(40))
-
-deposit_input = ("Deposit: D")
-print(deposit_input.center(40))
-
-withdraw_input = ("Withdraw: W")
-print(withdraw_input.center(40))
-
-quit_input = ("To Quit: Q")
-print(quit_input.center(40))
+print("Deposit: D".center(40))
+print("Withdraw: W".center(40))
+print("To Quit: Q".center(40))
 print(border)
-
-invalid_selection = "INVALID SELECTION"
-insufficient_funds = "INSUFFICIENT FUNDS"
 
 selection = input("Enter your selection: ")
 if selection not in interface_values:
     print(border)
-    print(invalid_selection.center(40))
+    print("INVALID SELECTION".center(40))
     print(border)
 else: transaction_amount = float(input("Enter amount of transaction: "))
 if transaction_amount < 0:
@@ -55,13 +40,16 @@ elif selection == "D":
 elif selection == "W":
     if transaction_amount > users_bank_balance:
         print(border)
-        print(insufficient_funds.center(40))
+        print("INSUFFICIENT_FUNDS".center(40))
         print(border)
     else:
         users_bank_balance - transaction_amount
         print(border)
         print(f"Your current balance is {users_bank_balance - transaction_amount: 2f}")
         print(border)
+
+
+
 
 
 
